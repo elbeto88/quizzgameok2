ee9604c4b7e4694060fa1c043a6dd2411fec4c78
DESKTOP-3HRLSTD\Jorge Riezco <neuronaeditorial@gmail.com>
2023-02-06 05:01:04 -0600

    no message

--- Assets/scrips/SeleccionAleatoria.cs
+++ Assets/scrips/SeleccionAleatoria.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
+public class SeleccionAleatoria : MonoBehaviour
+{
+	
 
     public int numeroPreguntas;
-    public int count = 0;
+	public int count = 0;
+	public int countPreguntas = 0; 
     public List<int> numerosAleatorios;
     public bool stop = false;
-    public GameObject[] preguntas;
-    
+	public GameObject[] preguntas;
+	public int points = 0; 
+	public GameObject panelFinal;
+	public TextMeshProUGUI textoPuntos;
 
     // Start is called before the first frame update
     void Start()
@@ -30,12 +38,56 @@ using UnityEngine;
         }
         
     }
-    	public void PreguntaActiva()
+	public void Opcioncorrecta(Button _boton)
+	{
+		StartCoroutine(OpcioncorrectaCoroutine(_boton));
+	}
+    
+	public void OpcionIncorrecta(Button _boton)
+	{
+		StartCoroutine (OpcionIncorrectaCoroutine(_boton));
+	}
+	
+	void Update()
+	{
+		if(preguntas.Length == countPreguntas)
+		{
+			panelFinal.SetActive(true);
+			textoPuntos.text = points.ToString();
+		}
+	}
+	
+	
+	
+	public void PreguntaActiva()
     	{
-    		preguntas[numerosAleatorios[0]].SetActive(true);
+    		if(countPreguntas!=0)
+    		{
+    		preguntas[numerosAleatorios[numeroPreguntas]].SetActive(false);
+	    		countPreguntas++;
+    		preguntas[numerosAleatorios[numeroPreguntas]].SetActive(true);
+    		}
+    		else
+    		{
+	    		preguntas[numerosAleatorios[numeroPreguntas]].SetActive(true);
+	    		countPreguntas++;
+    		}
  
     	}
-
+	public IEnumerator OpcioncorrectaCoroutine(Button _boton)
+	{
+		_boton.image.color = Color.green;
+		yield return new WaitForSeconds(1);
+	    points++;
+		PreguntaActiva();
+	}
+	public IEnumerator OpcionIncorrectaCoroutine(Button _boton)
+	{
+		_boton.image.color = Color.red;
+		yield return new WaitForSeconds(1);
+		PreguntaActiva();	
+	}
+}
     
 
 
